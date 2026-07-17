f17fd4afd982f63fd6b79acea85f0c1766530a90
Vishnu vardhan <pasamvishnuvardhan88@gmail.com>
2026-07-17 10:00:23 +0530

    Delete AdapterPatternExample directory

--- AdapterPatternExample/app/src/main/java/adapterpattern/PayPalGateway.java
+++
@@ -1,9 +0,0 @@
-package adapterpattern;
-
-public class PayPalGateway {
-
-    public void sendPayment(double amount) {
-        System.out.println("PayPal Payment of $" + amount + " processed.");
-    }
-
-}
--- AdapterPatternExample/app/src/main/java/adapterpattern/PaymentProcessor.java
+++
@@ -1,5 +0,0 @@
-package adapterpattern;
-
-public interface PaymentProcessor {
-    void processPayment(double amount);
-}
